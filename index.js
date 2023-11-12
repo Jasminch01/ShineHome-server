dc08217389d9f6b84bfff8c79d4e7654004faf43
jasmin <jasminchakma895@gmail.com>
2023-11-12 21:10:38 +0600

    ShineHome Server

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.yh8qk3b.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const serviceCollection = client.db('ShineHome').collection('Services');
+const bedroomCollection = client.db('ShineHome').collection('bedroom');
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    app.get('/services', async (req, res) => {
+        const result = await serviceCollection.find().toArray()
+        res.send(result)
+    })
+
+    //get rooms collection
+    app.get('/bedrooms', async (req, res) => {
+        const result = await bedroomCollection.find().toArray()
+        res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+  res.send("ShineHome server is running");
+});
+
+app.listen(port, () => {
+  console.log(`ShineHome Server is running port : ${port}`);
+});
