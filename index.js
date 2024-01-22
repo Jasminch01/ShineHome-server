f401b75e6e9ccab80c436e5be0447974070e947c
jasmin <jasminchakma895@gmail.com>
2024-01-22 20:30:45 +0600

    origin change

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5001;
 // middleware
 app.use(cors({
-  origin : ['http://localhost:5174',],
+  origin : ['http://localhost:5173',],
   credentials : true
 }));
 app.use(express.json());
