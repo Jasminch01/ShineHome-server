533ade21bab95a8d1d38ce0d26f0924809d2ec72
jasmin <jasminchakma895@gmail.com>
2024-01-22 16:50:48 +0600

    set origin

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 // middleware
-app.use(cors());
+app.use(cors({
+  origin : ['http://localhost:5174/'],
+  credentials : true
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.yh8qk3b.mongodb.net/?retryWrites=true&w=majority`;
