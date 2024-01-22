e38c05234b7be8f975090429d45ccf15dedaf583
jasmin <jasminchakma895@gmail.com>
2024-01-22 16:45:39 +0600

    attachment

--- index.js
+++ index.js
@@ -2296,21 +2296,73 @@ async function run() {
         </body>
       </html>    
 `;
-      try {
-        nodeoutlook.sendEmail({
-          auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS,
+      nodeoutlook.sendEmail({
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+        from: quoteInfo.form_email,
+        to: [quoteInfo.to_email[0], quoteInfo.to_email[1]],
+        subject: quoteInfo.subject,
+        html: emailTemplate,
+        replyTo: quoteInfo.form_email,
+        attachments: [
+          {
+            filename: "text1.txt",
+            content: "hello world!",
           },
-          from: quoteInfo.form_email,
-          to: [quoteInfo.to_email[0], quoteInfo.to_email[1]],
-          subject: quoteInfo.subject,
-          html: emailTemplate,
-          replyTo: quoteInfo.form_email,
-        });
-      } catch (error) {
-        console.log(error)
-      }
+          {
+            // binary buffer as an attachment
+            filename: "text2.txt",
+            content: new Buffer("hello world!", "utf-8"),
+          },
+          {
+            // file on disk as an attachment
+            filename: "text3.txt",
+            path: "/path/to/file.txt", // stream this file
+          },
+          {
+            // filename and content type is derived from path
+            path: "/path/to/file.txt",
+          },
+          {
+            // stream as an attachment
+            filename: "text4.txt",
+            content: fs.createReadStream("file.txt"),
+          },
+          {
+            // define custom content type for the attachment
+            filename: "text.bin",
+            content: "hello world!",
+            contentType: "text/plain",
+          },
+          {
+            // use URL as an attachment
+            filename: "license.txt",
+            path: "https://raw.github.com/nodemailer/nodemailer/master/LICENSE",
+          },
+          {
+            // encoded string as an attachment
+            filename: "text1.txt",
+            content: "aGVsbG8gd29ybGQh",
+            encoding: "base64",
+          },
+          {
+            // data uri as an attachment
+            path: "data:text/plain;base64,aGVsbG8gd29ybGQ=",
+          },
+          {
+            // use pregenerated MIME node
+            raw:
+              "Content-Type: text/plain\r\n" +
+              "Content-Disposition: attachment;\r\n" +
+              "\r\n" +
+              "Hello world!",
+          },
+        ],
+        onError: (e) => console.log(e),
+        onSuccess: (i) => console.log(i),
+      });
       res.send(result);
     });
 
